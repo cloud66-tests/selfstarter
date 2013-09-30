d7ffb466f82225cddbfd9b1c84db314c42989943
Philip Kallberg <philip@cloud66.com>
2013-09-30 16:04:36 +0100

    update

--- .cloud66/seed.sh
+++ .cloud66/seed.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-cd $RAILS_STACK_PATH
-rake db:seed
+FILE=/tmp/seed_done
+
+if [ -f $FILE ]
+then
+	echo "File $FILE exists..."
+else
+	cd $RAILS_STACK_PATH
+    rake db:seed
+    touch /tmp/seed_done
+fi
